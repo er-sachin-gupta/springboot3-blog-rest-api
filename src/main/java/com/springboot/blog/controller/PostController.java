c83d26c8b029e4daf74dc13fd50c4fddd9c829cb
Sachin Gupta <sgs.engineer@yahoo.co.in>
2023-07-14 16:14:56 +0530

    Spring Security initials Added

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
@@ -21,6 +22,7 @@ public class PostController {
     }
 
     // create blog post rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -44,6 +46,7 @@ public class PostController {
     }
 
     //update post by id rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable("id") long id) {
         PostDto postResponse = postService.updatePost(postDto, id);
@@ -51,6 +54,7 @@ public class PostController {
     }
 
     //delete post by id rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
         postService.deletePost(id);
