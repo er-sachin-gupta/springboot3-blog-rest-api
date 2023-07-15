6c3c005b9e342510dc8a3070be1900f7faae1ff6
Sachin Gupta <sgs.engineer@yahoo.co.in>
2023-07-15 09:27:19 +0530

    Category Controller and Service classes added

---
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -0,0 +1,57 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CategoryDto;
+import com.springboot.blog.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // Build Add Category Rest Api
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
+        CategoryDto savedCategory = categoryService.addCategory(categoryDto);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+
+    // Build get category rest api
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable("id") Long categoryId) {
+        return ResponseEntity.ok(categoryService.getCategory(categoryId));
+    }
+
+    // Build Get all Categories rest api
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    // Build Update Category Rest Api
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable("id") Long categoryId) {
+        CategoryDto updatedCategory = categoryService.updateCategory(categoryDto, categoryId);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    // Build delete category rest Api
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> deleteCategory(@PathVariable(value = "id") Long categoryId) {
+        categoryService.deleteCategory(categoryId);
+        return ResponseEntity.ok("Category deleted successfully");
+    }
+}
